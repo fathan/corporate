13a55e2d15cceb7adad7743402986e5f0ea3a8d2
FathanRohman <doctor.fathan@gmail.com>
2017-08-07 14:27:45 +0700

    revision

--- build/build.js
+++ build/build.js
@@ -6,12 +6,17 @@ var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
 var chalk = require('chalk')
+var shell = require('shelljs')
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
 
 var spinner = ora('building for production...')
 spinner.start()
+
+var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
+shell.rm('-rf', assetsPath)
+shell.config.silent = false
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
